5a9b1ba3d22db792a5aea0081328c79ec6b22ef7
Exstaa <exstagithub@gmail.com>
2024-05-02 19:26:13 +0400

    main class uploaded.

---
+++ java/az/hafizrzazade/studentdatabasesystem/StudentDatabaseApplication.java
@@ -0,0 +1,11 @@
+package az.hafizrzazade.studentdatabasesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDatabaseApplication {
+    public static void main(String[] args) {
+    	SpringApplication.run(StudentDatabaseApplication.class, args);
+    }
+}
